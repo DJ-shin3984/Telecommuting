1b745a0170ce6156bf23125f974adfd1d0d97db7
user <user@DESKTOP-6PJ4SE9>
2021-01-02 11:47:04 +0900

    부서 테이블 추가 / + exerd로 추가하는것이 아닌 table.sql 에서 추가 할수있도록 다시 전부 수정함

--- web/WEB-INF/table_schema/Telecommuting_Table.sql
+++ web/WEB-INF/table_schema/Telecommuting_Table.sql
@@ -1,5 +1,13 @@
 
 SELECT * FROM tab;
+SELECT * FROM USER_SEQUENCES;
+
+/* 부서 */
+CREATE TABLE DEPARTMENT (
+	D_IDX NUMBER(20) constraint D_PK primary key, /* 부서 번호 */
+	D_NAME VARCHAR2(20) NOT NULL, /* 부서 이름 */
+	D_PHONE VARCHAR2(20) NOT NULL /* 부서 연락처 */
+);
 
 /* 사원정보  */
 CREATE TABLE MEMBER (
@@ -12,55 +20,59 @@ CREATE TABLE MEMBER (
 	M_EMAIL VARCHAR2(40) NOT NULL, /* 이메일 */
 	M_ADD VARCHAR2(40) NOT NULL, /* 주소 */
 	M_EMPDATE DATE NOT NULL, /* 입사일 */
-	M_DEPARTMENT VARCHAR2(15) NOT NULL, /* 부서명 */
-	M_PHOTO VARCHAR2(100) NOT NULL /* 사진 */
+	D_IDX NUMBER(20) NOT NULL, /* 부서 번호 */
+	M_PHOTO VARCHAR2(100) NOT NULL, /* 사진 */
+	TOKEN VARCHAR2(200) NOT NULL, /* 토큰  */
+	CONSTRAINT MD_FK FOREIGN KEY(D_IDX) REFERENCES DEPARTMENT(D_IDX),
+	CONSTRAINT UK_MEMBER UNIQUE(M_ID, M_PHONE, M_EMAIL)
 );
-
-insert into member values(sq_midx.nextval,'신동재','admin','admin',
-					'010-1234-5678','010-2222-3333','n@n.com',
-						'서울',sysdate,'개발부','저장경로')
 
 /* 회의  */
 CREATE TABLE CONFERENCE (
-	C_IDX NUMBER(20) constraint M_PK primary key, /* 회의번호 */
-	C_TITLE VARCHAR2(50) NOT NULL /* 회의 제목 */
-	C_CONTENT VARCHAR2(200) NOT NULL /* 회의 내용 */
-	M_IDX NUMBER(20) constraint M_FK foreign key, /* 사원번호 */
+	C_IDX NUMBER(20) constraint C_PK primary key, /* 회의번호 */
+	C_TITLE VARCHAR2(50) NOT NULL, /* 회의 제목 */
+	C_CONTENT VARCHAR2(200) NOT NULL, /* 회의 내용 */
+	M_IDX NUMBER(20) NOT NULL, /* 사원번호 */
+	CONSTRAINT CM_FK FOREIGN KEY(M_IDX) REFERENCES MEMBER(M_IDX)
 );
 
 /* 게시판  */
 CREATE TABLE BOARD (
-	B_IDX NUMBER(20) constraint M_PK primary key, /* 게시판 번호 */
-	B_TITLE VARCHAR2(50) NOT NULL /* 게시판 제목 */
-	B_CONTENT VARCHAR2(200) NOT NULL /* 게시판 내용 */
-	M_IDX NUMBER(20) constraint M_FK foreign key, /* 사원 번호 */
+	B_IDX NUMBER(20) constraint B_PK primary key, /* 게시판 번호 */
+	B_TITLE VARCHAR2(50) NOT NULL, /* 게시판 제목 */
+	B_CONTENT VARCHAR2(200) NOT NULL, /* 게시판 내용 */
+	M_IDX NUMBER(20) NOT NULL, /* 사원 번호 */
+	CONSTRAINT BM_FK FOREIGN KEY(M_IDX) REFERENCES MEMBER(M_IDX)
 );
 
 /* 근태  */
 CREATE TABLE ATTENDANCE (
-	A_IDX NUMBER(20) constraint M_PK primary key, /* 근태 번호 */
-	A_GOTIME DATE NOT NULL /* 출근시간 */
-	A_OFFTIME DATE NOT NULL /* 퇴근시간 */
-	M_IDX NUMBER(20) constraint M_FK foreign key, /* 사원 번호 */
+	A_IDX NUMBER(20) constraint A_PK primary key, /* 근태 번호 */
+	A_GOTIME DATE NOT NULL, /* 출근시간 */
+	A_OFFTIME DATE NOT NULL, /* 퇴근시간 */
+	M_IDX NUMBER(20) NOT NULL, /* 사원 번호 */
+	CONSTRAINT AM_FK FOREIGN KEY(M_IDX) REFERENCES MEMBER(M_IDX)
 );
 
 /* 개인업무 */
 CREATE TABLE INDIVIDUAL_WORK (
-	IW_IDX NUMBER(20) constraint M_PK primary key, /* 개인업무 번호 */
-	IW_TITLE VARCHAR2(20) NOT NULL /* 개인업무 제목 */
-	IW_CONTENT VARCHAR2(100) NOT NULL /* 개인업무 내용 */
-	M_IDX NUMBER(20) constraint M_FK foreign key, /* 사원 번호 */
+	IW_IDX NUMBER(20) constraint I_PK primary key, /* 개인업무 번호 */
+	IW_TITLE VARCHAR2(20) NOT NULL, /* 개인업무 제목 */
+	IW_CONTENT VARCHAR2(100) NOT NULL, /* 개인업무 내용 */
+	M_IDX NUMBER(20) NOT NULL, /* 사원 번호 */
+	CONSTRAINT IWM_FK FOREIGN KEY(M_IDX) REFERENCES MEMBER(M_IDX)
 );
 
 /* 부서 업무 */
 CREATE TABLE DEPARTMENT_WORK (
-	DW_IDX NUMBER(20) constraint M_PK primary key, /* 부서 업무 번호 */
-	DW_TITLE VARCHAR2(20) NOT NULL /* 제목 */
-	DW_CONTENT VARCHAR2(100) NOT NULL /* 내용 */
-	DW_SCHEDULE VARCHAR2(100) NOT NULL /* 스케줄 */
-	DW_MONTHWORK VARCHAR2(100) NOT NULL /* 월간 업무 */
-	DW_WEEKWORK VARCHAR2(100) NOT NULL /* 주간 업무 */
-	M_IDX NUMBER(20) constraint M_FK foreign key, /* 사원 번호 */
+	DW_IDX NUMBER(20) constraint DW_PK primary key, /* 부서 업무 번호 */
+	DW_TITLE VARCHAR2(20) NOT NULL, /* 제목 */
+	DW_CONTENT VARCHAR2(100) NOT NULL, /* 내용 */
+	DW_SCHEDULE VARCHAR2(100) NOT NULL, /* 스케줄 */
+	DW_MONTHWORK VARCHAR2(100) NOT NULL, /* 월간 업무 */
+	DW_WEEKWORK VARCHAR2(100) NOT NULL, /* 주간 업무 */
+	M_IDX NUMBER(20), /* 사원 번호 */
+	CONSTRAINT DWM_FK FOREIGN KEY(M_IDX) REFERENCES MEMBER(M_IDX)
 );
 
   /* ------------------------------------------------------------- */
@@ -79,6 +91,9 @@ SELECT * FROM ATTENDANCE;
 
 /* 개인업무 테이블 보기 */
 SELECT * FROM INDIVIDUAL_WORK;
+
+/* 부서 테이블 보기 */
+SELECT * FROM DEPARTMENT;
 
 /* 부서 업무 테이블 보기 */
 SELECT * FROM DEPARTMENT_WORK;
@@ -100,6 +115,9 @@ DROP TABLE ATTENDANCE CASCADE CONSTRAINTS;
 /* 개인업무 삭제 */
 DROP TABLE INDIVIDUAL_WORK CASCADE CONSTRAINTS;
 
+/* 부서 삭제 */
+DROP TABLE DEPARTMENT CASCADE CONSTRAINTS;
+
 /* 부서 업무 삭제 */
 DROP TABLE DEPARTMENT_WORK CASCADE CONSTRAINTS;
 
@@ -119,6 +137,9 @@ CREATE SEQUENCE SQ_AIDX INCREMENT BY 1  START WITH 100 NOCYCLE NOCACHE; /* 사�
 
 /* 개인업무 시퀀스 생성 */
 CREATE SEQUENCE SQ_IWIDX INCREMENT BY 1  START WITH 100 NOCYCLE NOCACHE; /* 사원번호 생성 */
+
+/* 부서 시퀀스 생성 */
+CREATE SEQUENCE SQ_DIDX INCREMENT BY 1  START WITH 100 NOCYCLE NOCACHE; /* 사원번호 생성 */
 
 /* 부서 업무 시퀀스 생성 */
 CREATE SEQUENCE SQ_DWIDX INCREMENT BY 1  START WITH 100 NOCYCLE NOCACHE; /* 사원번호 생성 */
@@ -140,12 +161,18 @@ drop sequence SQ_AIDX;
 /* 개인업무 시퀀스 삭제 */
 drop sequence SQ_IWIDX;
 
+/* 부서 시퀀스 삭제 */
+drop sequence SQ_DIDX;
 /* 부서 업무 시퀀스 삭제 */
 drop sequence SQ_DWIDX;
 
   /* ------------------------------------------------------------- */
 
-
+insert into department values(sq_midx.nextval,'개발부','010-9999-8888');
+								
+insert into member values(sq_midx.nextval,'신동재','admin','admin',
+							'010-1234-5678','010-2222-3333','n@n.com',
+								'서울', sysdate, 105, '저장경로', 'TOKEN');
 
 
 
